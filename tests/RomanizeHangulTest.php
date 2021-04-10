4d481cae2c6e4df8713425bc337c53728f98da80
浦上賀裕 <gayou.ura@gmail.com>
2021-04-11 00:47:43 +0900

    #2 ユニットテスト導入

---
+++ tests/RomanizeHangulTest.php
@@ -0,0 +1,57 @@
+<?php namespace Gayou\Romanize\Test;
+
+use PHPUnit\Framework\TestCase;
+use Gayou\Romanize\RomanizeHangul;
+
+class RomanizeHangulTest extends TestCase
+{
+    public function testPersonNameByLee()
+    {
+        $hangul = new RomanizeHangul();
+
+        //イ・デホ
+        $this->assertSame($hangul->katakana('이대호', true), 'イ・デホ');
+
+        //イ・スンヨプ
+        $this->assertSame($hangul->katakana('이승엽', true), 'イ・スンヨプ');
+    }
+
+
+    public function testPersonNameByKim()
+    {
+        $hangul = new RomanizeHangul();
+
+        //キム・グァンヒョン
+        $this->assertSame($hangul->katakana('김광현', true), 'キム・グァンヒョン');
+
+        //キム・テギュン
+        $this->assertSame($hangul->katakana('김태균', true), 'キム・テギュン');
+    }
+
+
+    public function testPersonNameByPark()
+    {
+        $hangul = new RomanizeHangul();
+
+        //パク・ミンウ
+        $this->assertSame($hangul->katakana('박민우', true), 'パク・ミンウ');
+    }
+
+
+    public function testPersonNameByNa()
+    {
+        $hangul = new RomanizeHangul();
+
+        //ナ・ソンボム
+        $this->assertSame($hangul->katakana('나성범', true), 'ナ・ソンボム');
+    }
+
+
+    public function testPersonNameByYang()
+    {
+        $hangul = new RomanizeHangul();
+
+        //ヤン・ウイジ
+        $this->assertSame($hangul->katakana('양의지', true), 'ヤン・ウイジ');
+    }
+}
